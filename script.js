6351b8272171a84be40345c41a9e81a69334e380
amar-nath-dev <amarnathpraj331@gmail.com>
2025-05-28 23:38:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// For now, a simple alert on Hire Me button click
+document.getElementById('hireBtn').addEventListener('click', function (e) {
+    e.preventDefault();
+    alert('Fiverr Profile link is not available yet.');
+});
